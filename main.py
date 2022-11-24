9aeb05cef63e156cd52d7caa838be25907389f6b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-24 16:13:19 +0100

    init

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 print("python bootcamp");
 
 
-# bmi calcuator
-
-height = int(input("your height\n"))
-weight = int(input("your weight\n"))
-height = height / 100
-
-bmi = round(weight / ((height)**2), 1)
-
-print("Your BMI: \n" + str(bmi))
+# life in weeks
