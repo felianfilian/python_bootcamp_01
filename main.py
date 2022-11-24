e2cc30abfec8a06d92e022f3b1381d9b6a2e31d7
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-24 16:07:12 +0100

    bmi calculator - update

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ print("python bootcamp");
 
 # bmi calcuator
 
-
-
 height = int(input("your height\n"))
 weight = int(input("your weight\n"))
 height = height / 100
 
 bmi = round(weight / ((height)**2), 1)
 
-print(bmi)
+print("Your BMI: \n" + str(bmi))
