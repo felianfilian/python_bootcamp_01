28cc526bcdb7d9dd55e6d5c394944f5f47b129cf
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 21:33:04 +0100

    odd or even new Project setup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 print("python bootcamp");
 
+# odd or even
 
-# bmi calcuator
 
-height = int(input("your height\n"))
-weight = int(input("your weight\n"))
-height = height / 100
-
-bmi = round(weight / ((height)**2), 1)
-
-print("Your BMI: \n" + str(bmi))
