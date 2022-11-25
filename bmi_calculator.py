053349b97226280e716cbc96e2d2eca93ec31a0b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 21:56:51 +0100

    bmi calculator new version

---
+++ bmi_calculator.py
@@ -0,0 +1,7 @@
+# bmi calculator version 2
+height = float(input("your height"))
+weight = float(input("your weight"))
+
+bmi = weight / (height**2)
+
+print(bmi)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 print("python bootcamp");
 
-# odd or even
 
-num = int(input("your number\n"))
-if num % 2 == 0:
-    print("even number")
-else:
-    print("odd number")
+# bmi calculator version 2
+height = float(input("your height\n"))
+weight = float(input("your weight\n"))
+
+bmi = weight / ((height/100)**2)
+bmi = round(bmi)
+
+print("Your BMI:\n" + str(bmi))
