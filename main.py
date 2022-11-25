2262a36355314ff901fc2e907b696b0b08b507da
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 21:17:36 +0100

    updated types

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ print("python bootcamp");
 
 # tip calculator
 
-bill = int(input("what is the total bill\n"))
-tip = int(input("how much tip\n"))
+bill = float(input("what is the total bill\n"))
+tip = float(input("how much tip\n"))
 people = int(input("how many people\n"))
 bill_pp = bill * (1+(tip/100)) / people
 
-print(f"Each person pay: {bill_pp}")
+print(f"Each person pay: {round(bill_pp, 2)} €")
