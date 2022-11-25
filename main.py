83c171ee7ef383357218ab8841e77ed49811a2ff
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 21:26:32 +0100

    solve rounding error

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print("python bootcamp");
 bill = float(input("what is the total bill\n"))
 tip = float(input("how much tip\n"))
 people = int(input("how many people\n"))
-bill_pp = bill * (1+(tip/100)) / people
-
-print(f"Each person pay: {round(bill_pp, 2)} €")
+bill_pp = round(bill * (1+(tip/100)) / people)
+bill_pp = "{:.2f}".format(bill_pp)
+print(f"Each person pay: {bill_pp} €")
