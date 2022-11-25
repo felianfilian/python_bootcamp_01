ca831e9dc9f502c7dfc29202ebbc49c5f23b269d
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 22:30:08 +0100

    bmi result

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ bmi = weight / ((height/100)**2)
 bmi = round(bmi)
 
 print("Your BMI:\n" + str(bmi))
+
+if bmi <= 18.5:
+    print("you are underweight")
+elif bmi <= 25:
+    print("you are normal weight")
+elif bmi <= 30:
+    print("you are overweight")
+elif bmi <= 35:
+    print("you are obese")
+else:
+    print("your are clinical obese")
