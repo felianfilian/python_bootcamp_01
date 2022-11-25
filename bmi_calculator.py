f4c529538cee0f97219b34433d550f5351b4b961
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-25 22:34:45 +0100

    bmi calculator v2 finished

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,7 +1,21 @@
 # bmi calculator version 2
-height = float(input("your height"))
-weight = float(input("your weight"))
 
-bmi = weight / (height**2)
+def calc_bmi():
+    height = float(input("your height\n"))
+    weight = float(input("your weight\n"))
 
-print(bmi)
+    bmi = weight / ((height/100)**2)
+    bmi = round(bmi)
+
+    print("Your BMI:\n" + str(bmi))
+
+    if bmi <= 18.5:
+        print("you are underweight")
+    elif bmi <= 25:
+        print("you are normal weight")
+    elif bmi <= 30:
+        print("you are overweight")
+    elif bmi <= 35:
+        print("you are obese")
+    else:
+        print("your are clinical obese")
