e7badda40bb9c9c17b78f30b089650eb5744600e
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-26 17:13:31 +0100

    pizza order version 1

--- pizza_order.py
+++ pizza_order.py
@@ -1,2 +1,25 @@
 def pizza_order():
     print("Pizza Order")
+
+    size = input("pizza size (s/m/l):\n")
+    peperoni = input("add peperoni (y/n):\n")
+    cheese = input("extra cheese? (y/n)\n")
+
+    result = 0
+
+    if size == "s":
+        result += 15
+    elif size == "m":
+        result += 20
+    elif size == "m":
+        result += 25
+    else:
+        print("wrong size")
+
+    if peperoni == "y":
+        result += 2
+
+    if cheese == "y":
+        result += 1
+
+    print(f"pizza price = {result}")
