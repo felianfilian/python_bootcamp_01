08afbf799eb05975eebdd5b4e7df11c860998531
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-28 03:26:20 +0100

    rock paper update

--- rock_paper.py
+++ rock_paper.py
@@ -6,21 +6,22 @@ def rock_paper():
     sign = int(input("rock (0) paper (1) scissors (2)\n"))
     ai_sign = random.randint(0, 2)
 
-    print(signs[sign] + " VS " + signs[ai_sign] + "\n")
+    if sign >= 3 or sign < 0:
+        print("invalid number  - you loose")
+    else:
+        print(signs[sign] + " VS " + signs[ai_sign])
 
-    if sign == ai_sign:
-        print("draw")
-    elif sign == 0 and ai_sign == 1:
-        print("you loose")
-    elif sign == 0 and ai_sign == 2:
-        print("you win")
-    elif sign == 1 and ai_sign == 0:
-        print("you win")
-    elif sign == 1 and ai_sign == 2:
-        print("you loose")
-    elif sign == 2 and ai_sign == 0:
-        print("you loose")
-    elif sign == 2 and ai_sign == 1:
-        print("you win")
-
-    print(sign)
+        if sign == ai_sign:
+            print("draw")
+        elif sign == 0 and ai_sign == 1:
+            print("you loose")
+        elif sign == 0 and ai_sign == 2:
+            print("you win")
+        elif sign == 1 and ai_sign == 0:
+            print("you win")
+        elif sign == 1 and ai_sign == 2:
+            print("you loose")
+        elif sign == 2 and ai_sign == 0:
+            print("you loose")
+        elif sign == 2 and ai_sign == 1:
+            print("you win")
